0be45ac7b8accda80484298c4fdd3978dfb61b92
iliyan <iliyan.popov93@gmail.com>
2017-06-19 22:49:05 +0300

    Register and login logic are combined

---
+++ TetrisConsoleApplication/Tetris.Services/AuthenticationManager.cs
@@ -0,0 +1,47 @@
+﻿namespace Tetris.Services
+{
+    using System;
+    using Tetris.Models.Entities;
+
+    public static class AuthenticationManager
+    {
+        private static User _currentUser;
+
+    
+        public static bool IsAuthenticated()
+        {
+            return _currentUser != null;
+        }
+
+        public static void Logout()
+        {
+            if (!IsAuthenticated())
+            {
+                throw new InvalidOperationException("You should login first!");
+            }
+
+            _currentUser = null;
+        }
+
+    
+        public static void Login(User user)
+        {
+            //if (IsAuthenticated())
+            //{
+            //    throw new InvalidOperationException("You should logout first!");
+            //}
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("User to log in is invalid!");
+            }
+
+            _currentUser = user;
+        }
+
+        public static User GetCurrentUser()
+        {
+            return _currentUser;
+        }
+    }
+}
